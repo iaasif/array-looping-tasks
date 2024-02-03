c9726a0aeaa0c8284d2b7ad865e0c735391575c7
iaasif <imtiazasif.cse@gmail.com>
2024-02-04 01:36:14 +0600

    task4 done

---
+++ task4.js
@@ -0,0 +1,22 @@
+// ### Task 4 (Hard)
+
+// Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+
+// **Input:**
+// `const statement = 'I am a hard working person'`
+
+// **Output:**
+
+// `'person working hard a am I'`
+
+// ---
+
+const statement = 'I am a hard working person';
+
+
+
+let newStr = statement.split(" ");
+console.log(newStr);
+
+let revStr = newStr.reverse().join(" ");
+console.log(revStr);
