e12d7cb73a93f0734c559228414930ec42ed582f
iaasif <imtiazasif.cse@gmail.com>
2024-02-04 01:08:46 +0600

    task 2 done

--- task1.js
+++ task1.js
@@ -1,4 +1,18 @@
 // task1.js
+// ### Task 1
+
+// Write a JavaScript code to reverse the array colors `without using the reverse method`.
+
+// **Input:**
+// `const colors = ['red', 'blue', 'green', 'yellow', 'orange']`
+
+// **Output:**
+
+// `['orange', 'yellow', 'green', 'blue', 'red']`
+
+// ---
+
+
 const colors = ['red', 'blue', 'green', 'yellow', 'orange']
 
 // console.log(colors.reverse());
---
+++ task3.js
@@ -0,0 +1,12 @@
+// ### Task 3
+
+// Use a for...of loop to concatenate all the elements of an array into a single string.
+
+// ** Input:**
+//     `var numbers = ['Tom', 'Tim', 'Tin', 'Tik']`
+
+//     ** Output:**
+
+//         `'TomTimTinTik'`
+
+// ---
